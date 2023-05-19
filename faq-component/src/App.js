2e6312fe8b5aeee019b65a8f05033b0584b6fc2b
Enrique Cabrera <80559746+CadmiumRed40@users.noreply.github.com>
2023-05-19 16:50:23 -0400

    ..

--- faq-component/src/App.js
+++ faq-component/src/App.js
@@ -1,49 +1,52 @@
 import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-
+class App extends Component {
   state = {
-    activeIndex: 0
+    activeIndex: null
   };
 
-  handleItemClick = (index => {
-    this.setState({ activeIndex: index });
-  })
+  handleItemClick = (index) => {
+    this.setState((state) => ({
+      activeIndex: state.activeIndex === index ? null : index
+    }));
+  };
 
-  const FAQ = [
-    {
-      question: "How many bones does a cat have?",
-      answer: "A cat has 230 bones - 6 more than a human",
-    },
-    {
-      question: "How much do cats sleep?",
-      answer: "The average cat sleeps 12-16 hours per day",
-    },
-    {
-      question: "How long do cats live",
-      answer: "Outdoor cats live 5 years on average. Indoor\ncats live 15 years on average.",
-    },
-  ]
+  render() {
+    const FAQ = [
+      {
+        question: "How many bones does a cat have?",
+        answer: "A cat has 230 bones - 6 more than a human",
+      },
+      {
+        question: "How much do cats sleep?",
+        answer: "The average cat sleeps 12-16 hours per day",
+      },
+      {
+        question: "How long do cats live?",
+        answer: "Outdoor cats live 5 years on average. Indoor cats live 15 years on average.",
+      },
+    ];
 
-  const { activeIndex } = this.state;
+    const { activeIndex } = this.state;
 
-  return (
-    <div className="App">
-      <h1>Frequently Asked Questions</h1>
-      <div className='faqContainer'>
-        <button onClick={() => this.handleItemClick(index)}>&#10146;</button>
-        <ul>
-          {FAQ.map((faqItem, index) => (
-          <li key={index} className={index === 0 ? "active" : "inactive"}>
-            <span>{faqItem.question}</span>
-            <p>{faqItem.answer}</p>
-          </li>
-        ))}
-        </ul>
+    return (
+      <div className="App">
+        <h1>Frequently Asked Questions</h1>
+        <div className='faqContainer'>
+          <ul>
+            {FAQ.map((faqItem, index) => (
+              <li key={index} className={index === activeIndex ? "active" : "inactive"}>
+                <span>{faqItem.question}</span>
+                <button onClick={() => this.handleItemClick(index)}>&#10146;</button>
+                {index === activeIndex && <p>{faqItem.answer}</p>}
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
-
 
 export default App;
