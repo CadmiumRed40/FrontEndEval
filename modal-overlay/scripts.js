67ce83a6d41a5858b8d2448c7774a619365dab4e
Enrique Cabrera <80559746+CadmiumRed40@users.noreply.github.com>
2023-06-01 22:01:15 -0400

    working on modal visibility

---
+++ modal-overlay/scripts.js
@@ -0,0 +1,5 @@
+const click = document.getElementById('modalButton').addEventListener('click', () => { 
+    console.log('clicked!')
+})
+
+const changeVisibility = document.getElementsByClassName('inactive')
