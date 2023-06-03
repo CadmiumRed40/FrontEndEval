e15706c0ba2494ea65b5ea52b22096952bc05607
Enrique Cabrera <80559746+CadmiumRed40@users.noreply.github.com>
2023-06-03 13:25:45 -0400

    adding a button to close the modal

--- modal-overlay/scripts.js
+++ modal-overlay/scripts.js
@@ -1,10 +1,18 @@
 const modalContainer = document.getElementById("modalContainer");
 const modalButton = document.getElementById("modalButton");
+const closeModal = document.getElementById("closeModalButton")
 
 
 modalButton.addEventListener('click', () => { 
     console.log('clicked!')
     modalContainer.classList.toggle("active");
 })
+
+closeModal.addEventListener('click', () =>{
+    console.log('closed!')
+    modalContainer.classList.toggle("inactive");
+})
+
+
 
 
