15bf0277fabbd203ed6ed16c56833bc58fbdc269
Enrique Cabrera <80559746+CadmiumRed40@users.noreply.github.com>
2023-06-07 13:34:37 -0400

    creating logic for accepting offer and tinkering with the modal toggle

--- modal-overlay/scripts.js
+++ modal-overlay/scripts.js
@@ -2,6 +2,8 @@ const modalContainer = document.getElementById("modalContainer");
 const modalButton = document.getElementById("modalButton");
 const closeModal = document.getElementById("closeModalButton")
 const bodyContainer = document.getElementById("bodyContainer")
+const acceptedButton = document.getElementById("acceptOffer")
+const accepted = document.getElementById("AcceptedStatement")
 
 
 modalButton.addEventListener('click', () => { 
@@ -14,14 +16,18 @@ closeModal.addEventListener('click', () =>{
     modalContainer.classList.toggle("active");
 });
 
+
+
 bodyContainer.addEventListener('click', (e) =>{
     if (e.target === modalContainer){
     console.log('clicked on modal')
     } else {
         console.log('clicked outside modal')
-    modalContainer.classList.toggle("active");
+        modalContainer.classList.toggle("inactive");
     }
 });
+
+
 
 
 
