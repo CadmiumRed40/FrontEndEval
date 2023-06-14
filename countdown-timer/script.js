7a995162902c856a6cb80e308fc10c95d41954da
Enrique Cabrera <80559746+CadmiumRed40@users.noreply.github.com>
2023-06-13 23:58:36 -0400

    working out start,pause, and reset functions

--- countdown-timer/script.js
+++ countdown-timer/script.js
@@ -1,21 +1,52 @@
 const time = {
-    hour: (document.getElementById('hours')*3600),
-    minute: (document.getElementById('minutes')*60),
-    seconds: document.getElementById('seconds')
+    hour: document.getElementById('hours').value * 3600,
+    minute: document.getElementById('minutes').value * 60,
+    seconds: document.getElementById('seconds').value
 }
 
 //Possibly create an interface object to pull keys from 
-const setTime = Number(time[0]+time[1]+time[2])
+const setTime = Number(time.hour + time.minute + time.seconds)
 const startButton = document.getElementById('startButton')
 const pauseButton = document.getElementById('pauseButton')
 const resetButton = document.getElementById('resetButton')
 
-function startTime(setTime){
-    /**
-     * create a function that takes the set time and starts the timer. combine with the setTimeout function below.
-     */
-}
+let timerInterval;
 
-setTimeout(() =>{
-    console.log('time over.')
-}, setTime, 'Time Over!')
+/*===========Start Function==========*/
+startButton.addEventListener('click', () => {
+
+    document.getElementById('pauseButton').style.display = 'inline';
+    document.getElementById('resetButton').style.display = 'inline';
+
+    timerInterval = setInterval(() => {
+      const hours = Math.floor(setTime / 3600);
+      const minutes = Math.floor((setTime % 3600) / 60);
+      const seconds = setTime % 60;
+  
+      document.getElementById('timerDisplay').innerHTML = `${hours}:${minutes}:${seconds}`;
+  
+      if (setTime === 0) {
+        clearInterval(timerInterval);
+        document.getElementById('timerDisplay').innerHTML = 'Time over!';
+      }
+  
+      setTime--;
+    }, 1000);
+  });
+  
+  /*===========Pause Function==========*/
+  pauseButton.addEventListener('click', () => {
+    document.getElementById('pauseButton').style.display = 'none';
+    clearInterval(timerInterval);
+  });
+  
+  /*===========Reset Function==========*/
+  resetButton.addEventListener('click', () => {
+    document.getElementById('pauseButton').style.display = 'none';
+    document.getElementById('resetButton').style.display = 'none';
+    clearInterval(timerInterval);
+    setTime = Number(time.hour + time.minute + time.seconds);
+    document.getElementById('timerDisplay').innerHTML = `${time.hour}:${time.minute}:${time.seconds}`;
+  });
+
+
