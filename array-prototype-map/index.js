4bb14d75e24e282fb846f19aa5ead8c7506cb101
Enrique Cabrera <80559746+CadmiumRed40@users.noreply.github.com>
2023-06-21 22:24:55 -0400

    mapping function

---
+++ array-prototype-map/index.js
@@ -0,0 +1,9 @@
+function mapping(arr) {
+    const mappedArray = [];
+    for ( i = 0; arr.length > 0; i++) {
+    arr.map((e)=> { e * 10})
+    return mappedArray[i]
+    }
+    console.log(mappedArray)
+    return mappedArray
+}
