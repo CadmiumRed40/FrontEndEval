b8ad40ed17ac812b79b4f34c21c4c0c5663b3f11
Enrique Cabrera <80559746+CadmiumRed40@users.noreply.github.com>
2023-06-22 13:45:22 -0400

    forgot to push

--- array-prototype-map/index.js
+++ array-prototype-map/index.js
@@ -1,9 +1,9 @@
 function mapping(arr) {
     const mappedArray = [];
-    for ( i = 0; arr.length > 0; i++) {
-    arr.map((i)=> { i * 10})
-    return mappedArray[i]
+    for ( i = 0; arr.length > i; i++) {
+        arr.push(i)
     };
+    arr.map((i)=> { i * 10})
     console.log(mappedArray)
     return mappedArray
 }
